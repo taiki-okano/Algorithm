ded0030b92bd00b4d49e9927178461bb917d189c
taiki_okano <taiki_okano@me.com>
2016-07-30 00:23:48 +0900

    2016.7.13
    Solved AOJ0557
    Solved AOJ0227

--- AOJ/AOJ0227.cpp
+++ AOJ/AOJ0227.cpp
@@ -1,33 +1,54 @@
-#include <iostream>
+#include <cstdio>
 #include <algorithm>
 
-#define MAX_NM 1000
-
-int n, m, ans;
-int p[MAX_NM];
-
-void solve();
+constexpr int MAX_X = 4, MAX_Y = 50;
 
 int main(){
     while(true){
-        std::cin >> n >> m;
-        if(n == 0 && m == 0)
+        int X, Y, Z;
+        int roulette[MAX_X], jump[MAX_Y]{}, get_money[MAX_Y]{};
+        float dp[MAX_Y + 1][100 * MAX_Y + 1]{1};
+        scanf("%d%d%d", &X, &Y, &Z);
+        if(!X){
             break;
-        for(int i = 0; i < n; ++i)
-            std::cin >> p[i];
-        solve();
-        std::cout << ans << std::endl;
+        }
+        for(int i = 0; i < X; ++i){
+            scanf("%d", &roulette[i]);
+        }
+        for(int i = 0; i < Z; ++i){
+            int n, e, a;
+            scanf("%d%d%d", &n, &e, &a);
+            switch(e){
+                case 1:
+                    jump[n] = a;
+                    break;
+                case 2:
+                    get_money[n] = a;
+                    break;
+                case 3:
+                    get_money[n] = -a;
+                    break;
+            }
+        }
+        for(int i = 0; i < Y; ++i){
+            for(int j = 0; j <= 100 * MAX_Y; ++j){
+                if(dp[i][j] == 0){
+                    continue;
+                }
+                for(int k = 0; k < X; ++k){
+                    int ni, nj;
+                    ni = std::min(i + roulette[k], Y);
+                    nj = std::max(j + get_money[ni], 0);
+                    ni = std::min(ni + jump[ni], Y);
+                    dp[ni][nj] += dp[i][j] / X;
+                }
+            }
+        }
+        float ans = 0;
+        for(int i = 0; i <= 100 * MAX_Y; ++i){
+            ans += dp[Y][i] * i;
+        }
+        printf("%d\n", (int)ans);
     }
     return 0;
-}
-
-void solve(){
-    ans = 0;
-    std::sort(p, p + n, std::greater<int>());
-    for(int i = 1; i <= n; ++i){
-        if(i % m == 0)
-            continue;
-        ans += p[i - 1];
-    }
-    return;
 }
--- AOJ/AOJ0557.cpp
+++ AOJ/AOJ0557.cpp
@@ -1,25 +1,34 @@
-#include <iostream>
-#include <algorithm>
+#include <cstdio>
 
-constexpr int MAX_N = 100;
-long N, dp[MAX_N + 1][21];
-int num[MAX_N];
+constexpr int MAX_N = 300;
+constexpr int MAX_NUM = 20;
 
 int main(){
-    std::cin >> N;
-    for(int i = 0; i < N; ++i) {
-        std::cin >> num[i];
+    int N, num[MAX_N], result;
+    long dp[MAX_N][MAX_NUM + 1]{};
+    scanf("%d", &N); --N;
+    for(int i = 0; i < N; ++i){
+        scanf("%d", &num[i]);
     }
+    scanf("%d", &result);
     dp[0][num[0]] = 1;
-    
-    for(int i = 0; i < N - 2; ++i) {
-        for(int j = 0; j <= 20; ++j) {
-            if(j + num[i + 1] <= 20) dp[i + 1][j] += dp[i][j + num[i + 1]];
-            if(j - num[i + 1] >= 0) dp[i + 1][j] += dp[i][j - num[i + 1]];
+    for(int i = 1; i < N; ++i){
+        for(int j = 0; j <= MAX_NUM; ++j){
+            if(j >= num[i]){
+                dp[i][j] += dp[i - 1][j - num[i]];
+            }
+            if(j + num[i] <= MAX_NUM){
+                dp[i][j] += dp[i - 1][j + num[i]];
+            }
         }
     }
-    
-    std::cout << dp[N - 2][num[N - 1]] << std::endl;
-    
+    printf("%ld\n", dp[N - 1][result]);
+    /*
+    for(int i = 0; i < N; ++i){
+        for(int j = 0; j < 21; ++j){
+            printf("dp[%d][%d]: %ld\n", i, j, dp[i][j]);
+        }
+    }
+    */
     return 0;
 }
