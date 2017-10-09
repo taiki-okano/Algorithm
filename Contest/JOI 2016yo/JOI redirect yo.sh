df660835bd9b2142bd1d9f5e79725bf59549338b
taiki_okano <taiki_okano@me.com>
2017-10-09 12:25:48 +0900

    All Deleted

--- Contest/JOI 2016yo/JOI redirect yo.sh
+++
@@ -1,5 +0,0 @@
-./a.out < input$1/2016-yo-t$1-in1.txt > output$1/2016-yo-t$1-out1.txt
-./a.out < input$1/2016-yo-t$1-in2.txt > output$1/2016-yo-t$1-out2.txt
-./a.out < input$1/2016-yo-t$1-in3.txt > output$1/2016-yo-t$1-out3.txt
-./a.out < input$1/2016-yo-t$1-in4.txt > output$1/2016-yo-t$1-out4.txt
-./a.out < input$1/2016-yo-t$1-in5.txt > output$1/2016-yo-t$1-out5.txt
--- other/PCP 2016:8:8/Q2.cpp
+++
@@ -1,7 +0,0 @@
-#include <cstdio>
-
-constexpr int MAX_N = 10000, MAX_AB = 100000;
-
-int main(){
-    int N, A, B;
-}
