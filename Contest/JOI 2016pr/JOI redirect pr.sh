84e3a9b6c62a14d7b5d95c133d8c859a0f2dbe49
taiki_okano <taiki_okano@me.com>
2018-01-28 23:38:40 +0900

    2017.1.28
    Cleaned

--- Contest/JOI 2016pr/JOI redirect pr.sh
+++
@@ -1,6 +0,0 @@
-mkdir output
-./a.out < input$1/2016-pr-t$1-in1.txt > output$1/2016-pr-t$1-out1.txt
-./a.out < input$1/2016-pr-t$1-in2.txt > output$1/2016-pr-t$1-out2.txt
-./a.out < input$1/2016-pr-t$1-in3.txt > output$1/2016-pr-t$1-out3.txt
-./a.out < input$1/2016-pr-t$1-in4.txt > output$1/2016-pr-t$1-out4.txt
-./a.out < input$1/2016-pr-t$1-in5.txt > output$1/2016-pr-t$1-out5.txt
